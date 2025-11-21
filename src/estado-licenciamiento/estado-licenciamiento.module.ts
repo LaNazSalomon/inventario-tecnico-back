9a2157d6b19179d39701d1f3eacc1a90b2f49ddb
Salomon Larios Nazar <nazarsalomon123@gmail.com>
2025-11-21 01:24:28 -0600

    Correcion en el module de estado licenciamiento

--- src/estado-licenciamiento/estado-licenciamiento.module.ts
+++ src/estado-licenciamiento/estado-licenciamiento.module.ts
@@ -4,10 +4,11 @@ import { EstadoLicenciamientoController } from './estado-licenciamiento.controll
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EstadoLicenciamiento } from './entities/estado-licenciamiento.entity';
 import { EstadoFuncionamiento } from './entities/estado-funcionamiento.entity';
+import { EstadoFuncionamientoController } from './estado-funcionamiento.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([EstadoLicenciamiento, EstadoFuncionamiento])],
-  controllers: [EstadoLicenciamientoController],
+  controllers: [EstadoLicenciamientoController, EstadoFuncionamientoController],
   providers: [EstadoLicenciamientoService],
   exports: [TypeOrmModule],
 })
