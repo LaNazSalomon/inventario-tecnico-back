412981d6e0377ac581a489bb67cde0047ec07aee
Salomon Larios Nazar <nazarsalomon123@gmail.com>
2025-11-20 18:39:31 -0600

    CRUD cd unidad academica completada

--- src/unidad-academica/unidad-academica.service.ts
+++ src/unidad-academica/unidad-academica.service.ts
@@ -1,48 +1,119 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUnidadAcademicaDto } from './dto/create-unidad-academica.dto';
 import { UpdateUnidadAcademicaDto } from './dto/update-unidad-academica.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UnidadAcademica } from './entities/unidad-academica.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { ManejadorErroresDB } from 'src/common/helpers/ManejadorErroresDB';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { isUUID } from 'class-validator';
 
 @Injectable()
 export class UnidadAcademicaService {
   constructor(
     @InjectRepository(UnidadAcademica)
-    private readonly unidadRepository: Repository<UnidadAcademica>,
+    private readonly unidadAcademicaRepository: Repository<UnidadAcademica>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async create(createUnidadAcademicaDto: CreateUnidadAcademicaDto) {
     try {
-      const unidadAcademica = this.unidadRepository.create(
+      const unidadAcademica = this.unidadAcademicaRepository.create(
         createUnidadAcademicaDto,
       );
 
-      await this.unidadRepository.save(unidadAcademica);
+      await this.unidadAcademicaRepository.save(unidadAcademica);
       return unidadAcademica;
-      
     } catch (err) {
+      ManejadorErroresDB.erroresDB(err, 'Unidad Academica');
+    }
+  }
 
-      ManejadorErroresDB.erroresDB( err, 'Unidad Academica' );
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 50, offset = 0 } = paginationDto;
 
+    const unidadAcademica = await this.unidadAcademicaRepository.find({
+      take: limit,
+      skip: offset,
+    });
+
+    return unidadAcademica;
+  }
+
+  async findByTerm(term: string) {
+    let unidadAcademica: UnidadAcademica | UnidadAcademica[] | null;
+
+    if (isUUID(term)) {
+      unidadAcademica = await this.unidadAcademicaRepository.findOne({
+        where: { idUnidadAcademica: term },
+        relations: ['departamentos'], // importante si quieres traer los departamentos
+      });
+    } else {
+      const queryBuilder =
+        this.unidadAcademicaRepository.createQueryBuilder('unidad');
+
+      unidadAcademica = await queryBuilder
+        .leftJoinAndSelect('unidad.departamentos', 'departamento')
+        .where(
+          `(unidad.nombreUnidad ILIKE :term OR departamento.nombre ILIKE :term)`,
+          { term: `%${term}%` },
+        )
+        .getMany();
     }
 
+    if (
+      !unidadAcademica ||
+      (Array.isArray(unidadAcademica) && unidadAcademica.length === 0)
+    ) {
+      throw new NotFoundException('No se encontró ninguna unidad académica');
+    }
+
+    return unidadAcademica;
   }
 
-  findAll() {
-    return `This action returns all unidadAcademica`;
+  async update(id: string, updateDto: UpdateUnidadAcademicaDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      // Cargar la unidad existente y aplicar los cambios
+      const unidad = await this.unidadAcademicaRepository.preload({
+        idUnidadAcademica: id,
+        nombreUnidad: updateDto.nombreUnidad,
+      });
+
+      if (!unidad) {
+        throw new NotFoundException(
+          `No se encontró la unidad académica con ID ${id}`,
+        );
+      }
+
+      // Guardar cambios de la unidad
+      await queryRunner.manager.save(unidad);
+      await queryRunner.commitTransaction();
+
+      return `Unidad académica actualizada correctamente`;
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      ManejadorErroresDB.erroresDB(err, 'UnidadAcademicaService');
+    } finally {
+      await queryRunner.release();
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} unidadAcademica`;
-  }
+  async remove(id: string) {
+    try {
+      const unidadAcademica = await this.findByTerm(id);
 
-  update(id: number, updateUnidadAcademicaDto: UpdateUnidadAcademicaDto) {
-    return `This action updates a #${id} unidadAcademica`;
-  }
+      if (!unidadAcademica)
+        throw new NotFoundException('No se encontro ninguna unidad academica.');
 
-  remove(id: number) {
-    return `This action removes a #${id} unidadAcademica`;
+      await this.unidadAcademicaRepository.remove(
+        Array.isArray(unidadAcademica) ? unidadAcademica[0] : unidadAcademica,
+      );
+    } catch (err) {
+      ManejadorErroresDB.erroresDB(err, 'UnidadAcademicaService');
+    }
   }
 }
